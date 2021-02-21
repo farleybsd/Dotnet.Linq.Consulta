55c7e1d68da63793665b3a070694fe82ff831df3
farleybsd <farley.t.i@hotmail.com>
2021-02-21 10:19:14 -0300

    Linq com joins

---
+++ AluraTunes/AluraTunes/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluraTunes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Listar os gêneros com a palavra rock
+            var generos = new List<Genero>
+            {
+            new Genero { Id = 1, Nome = "Rock"},
+            new Genero { Id = 2, Nome =  "Reggae"},
+            new Genero { Id = 3, Nome =  "Rock Progressivo"},
+            new Genero { Id = 4, Nome =  "Punk"},
+            new Genero { Id = 5, Nome =  "Clássica"}
+            };
+
+            // Sem Linq
+            Imprimir(generos);
+
+            var query = from g in generos
+                        where g.Nome.Contains("Rock")
+                        select g;
+
+            Console.WriteLine();
+
+            // Com Linq
+            ImprimirGeneroLinq(query);
+
+            // Linq = Linguagem Integrated Query = Consulta integrada a linguagem
+
+            Console.ReadKey();
+        }
+
+        private static void ImprimirGeneroLinq(IEnumerable<Genero> query)
+        {
+            foreach (var genero in query)
+            {
+                Console.WriteLine("{0}\t{1}", genero.Id, genero.Nome);
+            }
+        }
+
+        private static void Imprimir(List<Genero> generos)
+        {
+            foreach (var genero in generos)
+            {
+                if(genero.Nome.Contains("Rock"))
+                Console.WriteLine("{0}\t{1}",genero.Id,genero.Nome);
+            }
+        }
+    }
+
+    class Genero
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ AluraTunes/Linq_Com_Joins/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Com_Joins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generos = new List<Genero>
+            {
+            new Genero { Id = 1, Nome = "Rock"},
+            new Genero { Id = 2, Nome =  "Reggae"},
+            new Genero { Id = 3, Nome =  "Rock Progressivo"},
+            new Genero { Id = 4, Nome =  "Punk"},
+            new Genero { Id = 5, Nome =  "Clássica"}
+            };
+
+           
+            // Listar Musicas
+
+            var musicas = new List<Musica>
+            {
+                new Musica { Id = 1, Nome = "Sweet Child O'Mine", GeneroId = 1 },
+                new Musica { Id = 2, Nome = "I Shoot The Sheriff", GeneroId = 2},
+                new Musica { Id = 3, Nome = "Danúbio Azul", GeneroId = 5},
+            };
+
+            var musicaQuery = from m in musicas
+                              join g in generos on m.GeneroId  equals g.Id
+                              select new {m,g };
+
+            foreach (var musica in musicaQuery)
+            {
+                Console.WriteLine("{0}\t{1}\t{2}", musica.m.Id, musica.m.Nome, musica.g.Nome);
+            }
+
+
+            Console.ReadKey();
+        }
+        
+    }
+
+    class Genero
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+
+    class Musica
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int GeneroId { get; set; }
+    }
+}
