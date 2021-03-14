1d78f92434baa07227a6a31711f6fbac9a32ed36
farleybsd <farley.t.i@hotmail.com>
2021-03-14 09:50:40 -0300

    3 - Linq to entities refinando consultas

--- AluraTunes/Linq_To_Entities/Program.cs
+++ AluraTunes/Linq_To_Entities/Program.cs
@@ -21,14 +21,14 @@ namespace Linq_To_Entities
                 ImprimirGenero(query);
 
                 var faixaEGeneros = from g in contexto.Generos
-                                   join f in contexto.Faixas
-                                   on g.GeneroId equals f.GeneroId
-                                   select new{f,g};
+                                    join f in contexto.Faixas
+                                    on g.GeneroId equals f.GeneroId
+                                    select new { f, g };
 
                 Console.WriteLine();
                 faixaEGeneros = faixaEGeneros.Take(10);
                 Console.WriteLine();
-                contexto.Database.Log = Console.WriteLine; // Depurar Sql
+                //contexto.Database.Log = Console.WriteLine; // Depurar Sql
 
                 foreach (var faixaEGenero in faixaEGeneros)
                 {
@@ -46,10 +46,10 @@ namespace Linq_To_Entities
                                   join alb in contexto.Albums
                                   on a.ArtistaId equals alb.ArtistaId
                                   where a.Nome.Contains(textoBusca)
-                                  select new 
-                                  { 
-                                    NomeArtista = a.Nome,
-                                    NomeAlbum = alb.Titulo
+                                  select new
+                                  {
+                                      NomeArtista = a.Nome,
+                                      NomeAlbum = alb.Titulo
                                   };
 
                 foreach (var item in queryBuscar)
@@ -64,20 +64,49 @@ namespace Linq_To_Entities
 
                 var queryLinqtoentitiessemjoin = from alb in contexto.Albums
                                                  where alb.Artista.Nome.Contains(textoBusca)
-                                                 select new { 
-                                                    NomeArtista= alb.Artista.Nome,
-                                                    NomeAlbum = alb.Titulo
+                                                 select new
+                                                 {
+                                                     NomeArtista = alb.Artista.Nome,
+                                                     NomeAlbum = alb.Titulo
 
                                                  };
 
                 foreach (var item in queryLinqtoentitiessemjoin)
                 {
-                    Console.WriteLine("{0}\t{1}", item.NomeArtista,item.NomeAlbum);
+                    Console.WriteLine("{0}\t{1}", item.NomeArtista, item.NomeAlbum);
                 }
+
+
+                Console.WriteLine();
+                //3 - Linq to entities refinando consultas
+                Console.WriteLine("3 - Linq to entities refinando consultas");
+
+                var buscarArtista = "Led Zeppelin";
+                var buscaAlbum = "Graffiti";
+
+                GetFaixas(contexto, buscarArtista, buscarArtista);
+
+                Console.ReadKey();
             }
 
 
-            Console.ReadKey();
+        }
+
+        private static void GetFaixas(AluraTunesEntities contexto, string buscarArtista,string buscaAlbum)
+        {
+            var queryLinqtoentitiesrefinandoconsultas = from f in contexto.Faixas
+                                                        where f.Album.Artista.Nome.Contains(buscarArtista)
+                                                        select f;
+
+            if (!string.IsNullOrEmpty(buscaAlbum))
+            {
+                queryLinqtoentitiesrefinandoconsultas = queryLinqtoentitiesrefinandoconsultas.Where(q => q.Album.Artista.Nome.Contains(buscaAlbum));
+            }
+
+            foreach (var faixa in queryLinqtoentitiesrefinandoconsultas)
+            {
+                Console.WriteLine("{0}\t{1}", faixa.Album.Titulo.PadRight(40), faixa.Nome);
+            }
         }
 
         private static void ImprimirGenero(IQueryable<Genero> query)
